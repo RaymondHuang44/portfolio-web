443d33d9eae9b316379048df829749f47ff02c6d
Raymond Huang <60895775+RaymondHuang44@users.noreply.github.com>
2024-11-21 01:52:56 -0800

    created navbar and styling

---
+++ web-react2/src/components/Navbar/Navbar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Nav, NavLink, NavMenu } from "./NavbarElements";
+
+const Navbar = () => {
+    return (
+        <>
+            <Nav>
+                <NavMenu>
+                    <NavLink to="/Home" activeStyle> Home </NavLink>
+                    <NavLink to="/projects" activeStyle> Projects </NavLink>
+                    <NavLink to="/about" activeStyle> About </NavLink>
+                    <NavLink to="/contact" activeStyle> Contact Me </NavLink>
+                    <NavLink to="/linkedin" activeStyle> LinkedIn </NavLink>
+                    <NavLink to="/github" activeStyle> GitHub </NavLink>
+                </NavMenu>
+            </Nav>
+        </>
+    );
+};
+
+export default Navbar;
