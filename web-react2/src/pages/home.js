3c46780b3a063e80ef47065f03f4fe3d6142f04f
Raymond Huang <60895775+RaymondHuang44@users.noreply.github.com>
2024-11-29 17:48:15 -0800

    added BIO to home page with image

--- web-react2/src/pages/home.js
+++ web-react2/src/pages/home.js
@@ -4,7 +4,7 @@ import "./style.css";
 function Home() {
     return (
         <div>
-            
+
             <section class="section">
                 <div class="box-main">
                     <div class="firstHalf">
@@ -13,35 +13,33 @@ function Home() {
                         </h1>
                         <br></br>
                         <p class="text-small">
-                            California State University - Fullerton Senior
+                        I am currently studying at California State University - Fullerton, pursuing a bachelor's degree in Computer Science. My hobbies include car restoration and modifications, 
+                        pc building and troubleshooting, and tinkering with things. I want to pursue full-stack development or create games.
+
                         </p>
                     </div>
                 </div>
             </section>
+
+            <div className="container">
+            <img 
+                src="linkedin looking ah ah.jpg" 
+                alt="Right Middle Image" 
+                className="box-main"
+                />
+            </div>
+
+
             <section class="section">
                 <div class="box-main">
-                    <div class="secondHalf">
-                        <h1 class="text-big" id="program">
-                            Languages
+                    <div class="firstHalf">
+                        <h1 class="text-big">
+                            That Random Fish In The Sea
                         </h1>
-                        <p class="text-small">
-                            C++, Python, JavaScript
-                        </p>
                     </div>
                 </div>
             </section>
-            <section class="section">
-                <div class="box-main">
-                    <div class="secondHalf">
-                        <h1 class="text-big" id="program">
-                          {/* add something */}
-                        </h1>
-                        <p class="text-small">
-                            {/* sample text */}
-                        </p>
-                    </div>
-                </div>
-            </section>
+
             <footer className="footer">
                 <p className="text-footer">
                     Created in 2024
@@ -52,5 +50,8 @@ function Home() {
         </div>
     );
 }
+
+
+
 
 export default Home;
